433eef91ebc822ecdfb2db149efdf833abc15ffd
Samantha Ostrowski <isphinxs@hotmail.com>
2021-03-31 21:10:21 -0400

    commit tree f9f93ca8808e5e8b15b0ae9481abb3b0af9304bb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+    validate :clickbait
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    def clickbait 
+        if title && !title.match(/(Won't Believe)|(Secret)|(Top \d+)|(Guess)/)
+            errors.add(:title, "error")
+        end
+    end
 end
